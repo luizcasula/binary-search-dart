a0c891d06f15a87726ac88d9da9caebe3000a8e3
Luiz Casula <luizcasulaf@gmail.com>
2020-05-08 13:35:50 -0400

    implement binary search

--- main.dart
+++ main.dart
@@ -1,4 +1,28 @@
-void main(){
-  print("teste");
+void main() {
+  List<int> array = [1, 2, 4, 5, 7, 8, 9, 10];
+  var test = binarySearch(array, 11);
+  print(test);
 }
 
+
+
+binarySearch(List<dynamic> array, var target) {
+  int left = 0;
+  int right = array.length - 1;
+
+  int middlePosition = (left + right) ~/ 2;
+
+  while (true) {
+    var middleElement = array[middlePosition];
+
+    if (middleElement == target) return middlePosition;
+
+    if (target < middleElement) right = middlePosition - 1;
+
+    if (target > middleElement) left = middlePosition + 1;
+
+    if (left > right) return -1;
+
+    middlePosition = (left + right) ~/ 2;
+  }
+}
